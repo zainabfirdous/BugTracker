9ae61b40e83069826f8eb6ac11bca1b1dbf18fcb
SachinDJ04 <128888007+SachinDJ04@users.noreply.github.com>
2024-01-17 18:06:03 +0530

    Second Commit

--- route/route.js
+++ route/route.js
@@ -6,78 +6,68 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 
-router.get("/", async(req,res)=>{
+router.get("/", async (req, res) => {
     try {
         // Fetch all employees from the database
         const employees = await employee.findAll();
-    
+
         // Render a template or send JSON response with the employees
         res.json(employees);
-      } catch (error) {
+    } catch (error) {
         console.error('Error fetching employees:', error);
         res.status(500).send('Internal Server Error');
-      }
+    }
 })
 
-router.post("/newEmployee", async(req, res)=>{
-    try{
+
+router.post("/newEmployee", async (req, res) => {
+    try {
         const body = req.body;
-        const newEmp = await employee.create(body);
+        const currentDate = new Date();
+        const formattedDate = currentDate.toISOString().split('T')[0];
+        const data = { ...body };
+        data.crtDate = formattedDate;
+        data.updDate = null;
+        const newEmp = await employee.create(data);
         res.json(newEmp);
-    }catch(error)
-    {
+    } catch (error) {
         console.error('Error creating employee:', error);
         res.status(500).send('Internal Server Error');
     }
 })
 
-router.post('/login', async(req,res)=>{
-        const username=req.body.userName;
-        const pass = req.body.password;
-        const user = await EmpProfile.findOne({
-            attributes: ['empID', 'userName', 'password'], // Specify the columns you need
-            where: { userName: username }
-        });
-    
-        if (user) {
-            // The user exists in the database.
-            console.log(user);
-            
-            // Checking if the password entered is matching the original password
-            if (user.password === pass) {
-                res.send('Login successful!');
-            } else {
-                // Password is incorrect.
-                res.send('Incorrect Password');
-            }
+router.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    const deletedCount = await employee.destroy({ where: { empID: id } });
+    res.json(deletedCount);
+})
+
+router.post('/login', async (req, res) => {
+    const username = req.body.userName;
+    const pass = req.body.password;
+    const user = await EmpProfile.findOne({
+        attributes: ['empID', 'userName', 'password'], // Specify the columns you need
+        where: { userName: username }
+    });
+
+    if (user) {
+        // The user exists in the database.
+        console.log(user);
+
+        // Checking if the password entered is matching the original password
+        if (user.password === pass) {
+            //   res.send('Login successful!');
+            res.json({ token: "thisismytoken" });
         } else {
-            // The user does not exist in the database
-            res.send('User not found');
+            // Password is incorrect.
+            res.send('Incorrect Password');
         }
-    })
+    } else {
+        // The user does not exist in the database
+        res.send('User not found');
+    }
+})
 
-// router.post('/login', passport.authenticate('local', {
-//     successRedirect: '/user',
-//     failureRedirect: '/login',
-//     failureFlash: true
-//   }));
-
-
-// function ensureAuthenticated(req, res, next) {
-//     if (req.isAuthenticated()) {
-//       return next();
-//     }
-//     res.redirect('/login');
-//   }
-  
-//   router.get('/user', ensureAuthenticated, (req, res) => {
-//     res.send('Welcome to the dashboard!');
-//   });
-
-//   router.get('/logout', (req, res) => {
-//     req.logout();
-//     res.redirect('/');
-//   });
 
 
 module.exports = router;
