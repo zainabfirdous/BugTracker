fd9ffa52c2389cd5bc9213125374bae9d3886576
SachinDJ04 <sachinpatidradj04@gmail.com>
2024-02-07 23:18:03 +0530

    Resetup in Index

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require("cors");
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser');
 const app = express();
 
 const {PORT, HOST} = require('./constant.js');
@@ -10,27 +8,10 @@ const router = require('./route/route.js');
 const bodyParser = require('body-parser');
 
 
-// app.use(cors(
-//     {
-//         origin: 'http://localhost:3000',
-//         methods: ["POST","GET","PUT","DELETE"],
-//         credentials : true
-//     }
-// ));
-app.use(bodyParser.json());
+app.use(cors());
 
-// app.use(cookieParser());
+//app.use(bodyParser.json());
 
-// app.use(session({
-//     secret : 'secret',
-//     resave: false,
-//     saveUninitialized : false,
-//     cookie: {
-//         sameSite: 'None',
-//         secure: false,
-//         maxAge: 1000*60*60*24
-//     },
-// }))
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use("/", router);
