4e3b5f1f829effb4ab5f2a1d7f065f6908973229
SachinDJ04 <sachinpatidradj04@gmail.com>
2024-02-07 22:55:38 +0530

    Done

--- bugtracker/src/Component/Desktop.js
+++ bugtracker/src/Component/Desktop.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 //import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useNavigate } from "react-router-dom";
 
-
+//done
 export default function Desktop() {
     const navigate = useNavigate();
 
