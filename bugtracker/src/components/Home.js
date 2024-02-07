fc46ac8c7c3982bbeca510a77027167048872b93
SachinDJ04 <sachinpatidradj04@gmail.com>
2024-02-08 00:23:07 +0530

    My Setup

--- bugtracker/src/components/Home.js
+++ bugtracker/src/components/Home.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function Desktop() {
-  axios.defaults.withCredentials = true;
+ // axios.defaults.withCredentials = true;
     const navigate = useNavigate();
 
     const handleLogin = () => {
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require("cors");
 
+
+// const jwt = require("jsonwebtoken");
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const jwtSecret = process.env.JWT_SECRET_KEY;
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+
+
 const app = express();
 
 const {PORT, HOST} = require('./constant.js');
-const router = require('./route/BaseRoute.js');
+const router = require('./route/route.js');
 
 const bodyParser = require('body-parser');
 
+app.use(cors());
 
+
+app.use(bodyParser.json());
 
 
 app.use(bodyParser.urlencoded({extended:false}));
