2d78771e3bb3dd288be8e59861c9e7e0281bcebe
Zainab Firdous <zainabfirdous786@gmail.com>
2024-02-07 23:51:23 +0530

    changed to comments

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const cors = require("cors");
 
-const jwt = require("jsonwebtoken");
-const dotenv = require('dotenv');
-dotenv.config();
-
+// const jwt = require("jsonwebtoken");
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const jwtSecret = process.env.JWT_SECRET_KEY;
 // const session = require('express-session');
 // const cookieParser = require('cookie-parser');
-main
+
 const app = express();
 
 const {PORT, HOST} = require('./constant.js');
-const router = require('./route/BaseRoute.js');
+const router = require('./route/route.js');
 
 const bodyParser = require('body-parser');
 
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
 
 
